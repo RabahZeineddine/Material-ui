76674699bf600567768786b68175ef7aeaf03693
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2021-03-24 14:48:56 -0300

    fix(components/index.ts): export PersistenDrawer component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
 export { default as Table } from './Table';
+
+export { default as PersistentDrawer } from './PersistentDrawer'
+export { default as DrawerPage } from './PersistentDrawer/DrawerPage'
