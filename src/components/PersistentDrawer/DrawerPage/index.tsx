95f11603af5bcf93fbe5816fdec0a2b8b928879f
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2021-03-24 14:22:37 -0300

    fix(drawerpage): fix unused variables

--- src/components/PersistentDrawer/DrawerPage/index.tsx
+++ src/components/PersistentDrawer/DrawerPage/index.tsx
@@ -1,8 +1,8 @@
 import { DrawerPageProps } from "config/@types/PersistentDrawer";
 import React from "react";
 
-const DrawerPage = React.forwardRef((props: DrawerPageProps, ref) => {
-  return null;
+const DrawerPage = React.forwardRef((props: DrawerPageProps) => {
+  return <div>{props}</div>;
 });
 
 export default DrawerPage;
