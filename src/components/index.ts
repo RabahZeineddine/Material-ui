c9fce505eb80f7e0570442e19eb781733ba6552e
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2021-04-05 21:52:35 -0300

    fix: export components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,12 @@
 export { default as Table } from './Table';
+export * from '../config/@types/'
 
 export { default as PersistentDrawer } from './PersistentDrawer'
 export { default as DrawerPage } from './PersistentDrawer/DrawerPage'
 
 export { default as FullScreenDialog } from './FullScreenDialog'
+
+export { default as SubmitButton } from './SubmitButton'
+
+export { default as Tabs } from './Tabs'
+export { default as Tab } from './Tabs/Tab'
---
+++ src/config/@types/index.ts
@@ -0,0 +1,4 @@
+
+
+
+export { TableColumn } from './Table'
